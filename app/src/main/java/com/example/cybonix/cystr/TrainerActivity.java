a57d095a2017650fd9ec9916cef7358ce0562605
Joseph Mark Orimoloye <cybonix@gmail.com>
2017-08-18 19:20:10 +0200

    This should be my first commit.

---
+++ app/src/main/java/com/example/cybonix/cystr/TrainerActivity.java
@@ -0,0 +1,13 @@
+package com.example.cybonix.cystr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trainer);
+    }
+}
